46f222c7c50809629f05463814f522e895a4a297
Lewie <lewie@Lewies-MacBook-Pro.local>
2021-07-20 09:12:38 +0200

    conversions

--- utils_test.py
+++ utils_test.py
@@ -1,2 +1,23 @@
 import numpy as np
 
+from utils import *
+
+
+def test_adjmatrix_to_adjdict():
+    adjmatrix = np.array([
+        [0, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0]
+    ])
+    assert adjmatrix_to_adjdict(adjmatrix) == {0: [1], 1: [0, 2], 2: [1]}
+
+
+def test_adjdict_to_adjmatrix():
+    adjdict = {0: [1], 1: [0, 2], 2: [1]}
+    assert np.array_equal(
+            adjdict_to_adjmatrix(adjdict), np.array([[0, 1, 0],
+                                                     [1, 0, 1],
+                                                     [0, 1, 0]])
+            )
+
+
